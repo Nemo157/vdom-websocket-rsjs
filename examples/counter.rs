d0c46b6321b9e46fde13c032c49bb28d9d2fce43
Wim Looman <wim@nemo157.com>
2018-06-23 13:16:23 +0200

    Pull initial framework out of coap-browse

---
+++ examples/counter.rs
@@ -0,0 +1,124 @@
+#[macro_use]
+extern crate serde_derive;
+
+extern crate futures;
+extern crate tokio;
+extern crate tokio_core;
+extern crate vdom_rsjs;
+extern crate vdom_websocket_rsjs;
+extern crate serde;
+
+use std::time::{Instant, Duration};
+use std::sync::Arc;
+
+use vdom_rsjs::{VNode, VTag, VProperty};
+use vdom_rsjs::render::{Render, Cache, TopCache};
+use vdom_websocket_rsjs::Action;
+use tokio::timer::Delay;
+use tokio_core::reactor::{Core, Handle};
+
+use futures::{Sink, Stream, Future, future::{self, Either}};
+use futures::sync::mpsc;
+
+#[derive(Serialize, Deserialize, Debug)]
+enum ActionTag {
+    Increment,
+    Decrement,
+}
+
+#[derive(Debug)]
+struct State {
+    count: usize,
+    handle: Handle,
+    actions: mpsc::Sender<Action<ActionTag>>,
+}
+
+impl State {
+    fn spawn(handle: Handle) -> (impl Sink<SinkItem = Action<ActionTag>, SinkError = ()>, impl Stream<Item = Arc<VNode<Action<ActionTag>>>, Error = ()>) {
+        let (action_tx, action_rx) = mpsc::channel(1);
+        let (render_tx, render_rx) = mpsc::channel(1);
+        let mut cache = TopCache::new();
+        let mut state = Arc::new(State {
+            count: 0,
+            handle: handle.clone(),
+            actions: action_tx.clone(),
+        });
+        handle.spawn(render_tx.send(cache.render(state.clone()))
+            .map_err(|e| println!("state send error: {:?}", e))
+            .and_then(|tx| action_rx
+                .fold(tx, move |tx, action| {
+                    let new_state = state.update(action);
+                    if !Arc::ptr_eq(&state, &new_state) {
+                        state = new_state;
+                        Either::A(tx.send(cache.render(state.clone())).map_err(|e| println!("state send error: {:?}", e)))
+                    } else {
+                        Either::B(future::ok(tx))
+                    }
+                })
+                .map(|_| ())));
+        (action_tx.sink_map_err(|e| println!("error sinking action: {:?}", e)), render_rx)
+    }
+
+    fn update(&self, action: Action<ActionTag>) -> Arc<State> {
+        let count = match action.tag {
+            ActionTag::Increment => self.count + 1,
+            ActionTag::Decrement => self.count - 1,
+        };
+
+        if count % 2 == 0 {
+            let reaction = Action::new(match action.tag {
+                ActionTag::Increment => ActionTag::Decrement,
+                ActionTag::Decrement => ActionTag::Increment,
+            });
+            let delay = Instant::now() + Duration::from_millis(200);
+            let actions = self.actions.clone();
+            self.handle.spawn(
+                Delay::new(delay)
+                    .map_err(|e| println!("error waiting to send reaction: {:?}", e))
+                    .and_then(|()| actions.send(reaction)
+                        .map(|_| ())
+                        .map_err(|e| println!("error sending reaction: {:?}", e))));
+        }
+
+        Arc::new(State {
+            count,
+            handle: self.handle.clone(),
+            actions: self.actions.clone(),
+        })
+    }
+}
+
+impl Render<Action<ActionTag>> for State {
+    fn render(&self, _cache: &mut Cache<Action<ActionTag>>) -> VNode<Action<ActionTag>> {
+        VTag::new("div")
+            .child(self.count.to_string())
+            .child(VTag::new("br"))
+            .child(VTag::new("button")
+                .prop("onclick", VProperty::Action(Action::new(ActionTag::Increment)))
+                .child("increment"))
+            .child(VTag::new("button")
+                .prop("onclick", VProperty::Action(Action::new(ActionTag::Decrement)))
+                .child("decrement"))
+            .into()
+    }
+}
+
+fn new_client(handle: Handle) -> (impl Sink<SinkItem = Action<ActionTag>, SinkError = ()>, impl Stream<Item = Arc<VNode<Action<ActionTag>>>, Error = ()>) {
+    let (actions, renders) = State::spawn(handle);
+
+    let actions = actions.with(|e| -> Result<Action<ActionTag>, ()> { println!("action: {:?}", e); Ok(e) });
+    let renders = renders.map(|r| { println!("render: {:?}", r); r });
+
+    (actions.sink_map_err(|e| println!("error sinking action: {:?}", e)), renders)
+}
+
+fn main() {
+    let mut core = Core::new().unwrap();
+    let handle = core.handle();
+
+    let server = vdom_websocket_rsjs::serve(
+        handle.clone(),
+        move || new_client(handle.clone()));
+
+    core.run(server).unwrap();
+}
---
+++ src/lib.rs
@@ -0,0 +1,124 @@
+#[macro_use]
+extern crate serde_derive;
+
+extern crate futures;
+extern crate serde;
+extern crate serde_json;
+extern crate tokio_core;
+extern crate vdom_rsjs;
+extern crate websocket;
+
+use std::sync::Arc;
+use std::fmt::Debug;
+use std::collections::HashMap;
+use std::borrow::Cow;
+
+use websocket::message::OwnedMessage;
+use websocket::server::InvalidConnection;
+use websocket::async::Server;
+
+use tokio_core::reactor::Handle;
+use futures::{Future, Sink, Stream, stream};
+use vdom_rsjs::VNode;
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+struct FullUpdate<A> {
+    tree: Arc<VNode<A>>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Action<Tag> {
+    pub tag: Tag,
+    associated: HashMap<String, String>,
+}
+
+impl<Tag> Action<Tag> {
+    pub fn new(tag: Tag) -> Action<Tag> {
+        Action { tag, associated: HashMap::new() }
+    }
+
+    pub fn associate(mut self, name: impl Into<Cow<'static, str>>, prop: impl Into<Cow<'static, str>>) -> Action<Tag> {
+        self.associated.insert(name.into().into_owned(), prop.into().into_owned());
+        self
+    }
+}
+
+pub fn serve<ActionTag, ClientSink, ClientStream, NewClient>(handle: Handle, mut new_client: NewClient) -> impl Future<Item = (), Error = ()>
+where ActionTag: Serialize + for<'a> Deserialize<'a> + Debug,
+      ClientSink: Sink<SinkItem = Action<ActionTag>, SinkError = ()> + 'static,
+      ClientStream: Stream<Item = Arc<VNode<Action<ActionTag>>>, Error = ()> + 'static,
+      NewClient: FnMut() -> (ClientSink, ClientStream) + Clone + 'static,
+{
+    let server = Server::bind("127.0.0.1:8080", &handle).unwrap();
+
+    server.incoming()
+        .map_err(|InvalidConnection { error, .. }| error)
+        .for_each(move |(upgrade, addr)| {
+            println!("Got a connection from {}", addr);
+
+            if !upgrade.protocols().iter().any(|s| s == "vdom-websocket-rsjs") {
+                spawn_future(upgrade.reject(), "Upgrade Rejection", &handle);
+                return Ok(());
+            }
+
+            let (client_sink, client_stream) = new_client();
+            let f = upgrade
+                .use_protocol("vdom-websocket-rsjs")
+                .accept()
+                .map_err(|e| println!("error accepting stream: {:?}", e))
+                .and_then(move |(ws, _)| {
+                    let (ws_sink, ws_stream) = ws.split();
+                    let incoming = ws_stream
+                        .take_while(|m| Ok(!m.is_close()))
+                        .filter_map(|m| match m {
+                            OwnedMessage::Ping(_) => {
+                                // TODO: Handle pings, going to need to
+                                // change these stream/sink pairs to have a
+                                // multiplexer in between them to allow
+                                // bypassing the client for sending the PONG
+                                // response.
+                                None
+                            }
+                            OwnedMessage::Pong(_) => None,
+                            OwnedMessage::Text(msg) => {
+                                match serde_json::from_str(&msg) {
+                                    Ok(action) => Some(action),
+                                    Err(err) => {
+                                        println!("error deserializing {:?}", err);
+                                        None
+                                    }
+                                }
+                            }
+                            OwnedMessage::Binary(_) => {
+                                println!("unexpected binary message");
+                                None
+                            }
+                            OwnedMessage::Close(_) => {
+                                None
+                            }
+                        })
+                        .map_err(|e| println!("error handling ws_stream: {:?}", e))
+                        .forward(client_sink.sink_map_err(|e| println!("error handling client_sink: {:?}", e)));
+                    let outgoing = client_stream
+                        .map_err(|e| println!("error on client_stream: {:?}", e))
+                        .map(|tree| serde_json::to_string(&FullUpdate { tree }).unwrap())
+                        .map(|json| OwnedMessage::Text(json))
+                        .chain(stream::once(Ok(OwnedMessage::Close(None))))
+                        .forward(ws_sink.sink_map_err(|e| println!("error on ws_sink: {:?}", e)));
+                    incoming.join(outgoing)
+                });
+
+            spawn_future(f, "Client Status", &handle);
+            Ok(())
+        })
+        .map_err(|err| println!("Server error: {:?}", err))
+}
+
+fn spawn_future<F, I, E>(f: F, desc: &'static str, handle: &Handle)
+    where F: Future<Item = I, Error = E> + 'static,
+          E: Debug
+{
+    handle.spawn(f.map_err(move |e| println!("{}: '{:?}'", desc, e))
+                  .map(move |_| println!("{}: Finished.", desc)));
+}
